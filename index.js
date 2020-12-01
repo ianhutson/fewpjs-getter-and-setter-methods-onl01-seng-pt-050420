bba4249dbf5cd0d65962fcd062c77871dad9f797
Ian Hutson <ih00209@georgiasouthern.edu>
2020-12-01 17:40:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ class Circle {
 {
   this.radius=radius;
 }
-  get radius()
-  return `${this.side} 
-}
+  get diameter() {
+    return this.radius * 2
+  }
+
+  set diameter( diameter ) {
+    this.radius = diameter / 2
+  }
+
+  get circumference() {
+    return ( this.radius * 2 ) * pi
+  }
+
+  set circumference( circumference ) {
+    this.radius = circumference / ( pi * 2 )
+  }
+
+  get area() {
+    return pi * ( this.radius * this.radius )
+  }
+
+  set area( area ) {
+    this.radius = Math.sqrt( area / pi )
+  }
